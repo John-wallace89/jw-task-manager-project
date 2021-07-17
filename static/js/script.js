4afcca30b76d693e75c2c2148177600771ad9f0a
John-wallace89 <john.wallace@heineken.co.uk>
2021-07-17 09:09:27 +0000

    Added accordion section

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,9 @@
+// Side nav JQuery
 $(document).ready(function(){
     $('.sidenav').sidenav();
+  });
+
+  // Accordion JQuery
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
   });
